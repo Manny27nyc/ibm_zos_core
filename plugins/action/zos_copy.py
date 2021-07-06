9eb79566099114a23b30b5459caae392a1205c85
ddimatos <dimatos@gmail.com>
2021-07-05 18:15:10 -0700

    Update the zos_copy module to use the ssh plugin for transport
    Signed-off-by: ddimatos <dimatos@gmail.com>

--- plugins/action/zos_copy.py
+++ plugins/action/zos_copy.py
@@ -25,6 +25,7 @@ from ansible.module_utils._text import to_bytes, to_text
 from ansible.module_utils.six import string_types
 from ansible.module_utils.parsing.convert_bool import boolean
 from ansible.plugins.action import ActionBase
+from ansible.utils.display import Display
 
 from ansible_collections.ibm.ibm_zos_core.plugins.module_utils.data_set import (
     is_member,
@@ -33,11 +34,6 @@ from ansible_collections.ibm.ibm_zos_core.plugins.module_utils.data_set import (
 )
 
 from ansible_collections.ibm.ibm_zos_core.plugins.module_utils import encode
-
-# ###### SSH POC IMPORTS
-from ansible.utils.display import Display
-from ansible.plugins.loader import connection_loader
-from io import StringIO
 
 display = Display()
 
@@ -123,9 +119,9 @@ class ActionModule(ActionBase):
                 msg = "Cannot specify 'mode', 'owner' or 'group' for MVS destination"
                 return self._exit_action(result, msg, failed=True)
 
-        if not isinstance(sftp_port, int) or not 0 < sftp_port <= 65535:
-            msg = "Invalid port provided for SFTP. Expected an integer between 0 to 65535."
-            return self._exit_action(result, msg, failed=True)
+        # if not isinstance(sftp_port, int) or not 0 < sftp_port <= 65535:
+        #     msg = "Invalid port provided for SFTP. Expected an integer between 0 to 65535."
+        #     return self._exit_action(result, msg, failed=True)
 
         if (not force) and self._dest_exists(src, dest, task_vars):
             return self._exit_action(result, "Destination exists. No data was copied.")
@@ -150,7 +146,7 @@ class ActionModule(ActionBase):
                 try:
                     local_content = _write_content_to_temp_file(content)
                     transfer_res = self._copy_to_remote(
-                        local_content, sftp_port, ignore_stderr=ignore_sftp_stderr
+                        local_content, ignore_stderr=ignore_sftp_stderr
                     )
                 finally:
                     os.remove(local_content)
@@ -173,7 +169,7 @@ class ActionModule(ActionBase):
                         )
                     task_args["size"] = os.stat(src).st_size
                 transfer_res = self._copy_to_remote(
-                    src, sftp_port, is_dir=is_src_dir, ignore_stderr=ignore_sftp_stderr
+                    src, is_dir=is_src_dir, ignore_stderr=ignore_sftp_stderr
                 )
 
             temp_path = transfer_res.get("temp_path")
@@ -220,72 +216,35 @@ class ActionModule(ActionBase):
 
         return _update_result(is_binary, copy_res, self._task.args)
 
-    def _copy_to_remote(self, src, port, is_dir=False, ignore_stderr=False):
+    def _copy_to_remote(self, src, is_dir=False, ignore_stderr=False):
         """Copy a file or directory to the remote z/OS system """
 
-        ansible_user = self._play_context.remote_user
-        ansible_host = self._play_context.remote_addr
         temp_path = "/{0}/{1}".format(gettempprefix(), _create_temp_path_name())
-        cmd = ["sftp", "-oPort={0}".format(port), ansible_user + "@" + ansible_host]
-        stdin = "put -r {0} {1}".format(src.replace("#", "\\#"), temp_path)
-
-        # ######################################################################
-        # Lets try to use the connection plugin to manage the SFTP command
-        # ######################################################################
-
-        new_stdin = StringIO()
-        conn = connection_loader.get('ssh', self._play_context, new_stdin)
-        ssh_cmd = conn._build_command('ssh', 'ssh')
-        sftp_cmd = conn._build_command('ssh', 'ssh','sftp')
-        display.vvv(u"VERBOSE Loaded connection plugin")
-        display.vvv(u"VERBOSE SSH command: {0} ".format(ssh_cmd))
-        display.vvv(u"VERBOSE SFTP command: {0} ".format(sftp_cmd))
-
-        display.vvv(u"VERBOSE OLD SSH cmd: {0} ".format(cmd))
-        display.vvv(u"VERBOSE Old STDIN: {0} ".format(stdin))
+        _src = src.replace("#", "\\#")
+        _sftp_action = 'put'
 
         if is_dir:
             src = src.rstrip("/") if src.endswith("/") else src
             base = os.path.basename(src)
             self._connection.exec_command("mkdir -p {0}/{1}".format(temp_path, base))
+            _sftp_action += ' -r' #add '-r` to clone the source trees
 
-        src_path = src.replace("#", "\\#")
-        remote_path = temp_path
-        (returncode, stdout, stderr)  = self._connection.put_file(src_path, remote_path)
+        display.vvv(u"{0} {1} TO {2}".format(_sftp_action, _src, temp_path), host=self._play_context.remote_addr)
+        (returncode, stdout, stderr) = self._connection._file_transport_command(_src, temp_path, _sftp_action)
+
         err = _detect_sftp_errors(stderr)
 
-        display.vvv(u"VERBOSE src_path: {0} ".format(src_path))
-        display.vvv(u"VERBOSE remote_path: {0} ".format(remote_path))
-        display.vvv(u"VERBOSE returncode: {0} ".format(returncode))
-        display.vvv(u"VERBOSE connection_command: {0} ".format(stdout))
-        display.vvv(u"VERBOSE connection_command: {0} ".format(stderr))
-
-        # ######################################################################
-        # END the connection plugin to manage the SFTP command
-        # ######################################################################
-
-        # if is_dir:
-        #     src = src.rstrip("/") if src.endswith("/") else src
-        #     base = os.path.basename(src)
-        #     self._connection.exec_command("mkdir -p {0}/{1}".format(temp_path, base))
-        # else:
-        #     stdin = stdin.replace(" -r", "", 1)
-        # transfer_data = subprocess.Popen(
-        #     cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        # )
-        # out, err = transfer_data.communicate(to_bytes(stdin))
-        # err = _detect_sftp_errors(err)
-
-        # if transfer_data.returncode != 0 or (err and not ignore_stderr):
-        #     return dict(
-        #         msg="Error transfering source '{0}' to remote z/OS system".format(src),
-        #         rc=transfer_data.returncode,
-        #         stderr=err,
-        #         stderr_lines=err.splitlines(),
-        #         failed=True,
-        #     )
+        if returncode != 0 or (err and not ignore_stderr):
+            return dict(
+                msg="Error transfering source '{0}' to remote z/OS system".format(src),
+                rc=returncode,
+                stderr=err,
+                stderr_lines=err.splitlines(),
+                failed=True,
+            )
 
         return dict(temp_path=temp_path)
+
 
     def _remote_cleanup(self, dest, dest_exists, task_vars):
         """Remove all files or data sets pointed to by 'dest' on the remote
